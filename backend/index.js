1edd0455fc47412049e301530d1586692730607a
ShivamMohit <smsingh0000@gmail.com>
2025-06-14 16:21:15 +0530

    Create a test api route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import userRouter from './routes/user.route.js';
+
 
 const app = express();
 
@@ -16,7 +18,13 @@ mongoose
     .catch((err) => {
     console.error("Error connecting to MongoDB:", err);
     })
+    
+app.use("/api/user", userRouter);
 
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -17,6 +17,7 @@ const userSchema = new mongoose.Schema(
       required: true,
     },
     
+    
   },
   { timestamps: true }
 );
