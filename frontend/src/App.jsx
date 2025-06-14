ad65384e2772c12d67931c6721885bdd55f8e60a
ShivamMohit <smsingh0000@gmail.com>
2025-06-14 15:20:19 +0530

    Create Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import './App.css'
+import { Signin } from './pages/Signin'
+import { Home } from './pages/Home'
+import { SignOut } from './pages/SignOut'
+import { Profile } from './pages/Profile'
+import { About } from './pages/About'
 
-export const App = () => {
+function App() {
+  
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/sign-in' element={<Signin />}/>
+        <Route path='/sign-out' element={<SignOut />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/profile' element={<Profile />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
+
+export default App
