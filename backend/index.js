01333420960452fb19be71b54222dada7f2e73bd
ShivamMohit <smsingh0000@gmail.com>
2025-06-14 16:08:24 +0530

    DB configure

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,23 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+    console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+    })
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
